8dc2edf59650e763eed97507e03002838100477d
Kevin Du <kevinkldu@gmail.com>
2016-06-26 10:23:02 -0700

    Update Flatten List.py

--- Flatten List.py
+++ Flatten List.py
@@ -1,3 +1,4 @@
 # Can be used to flatten nested lists of any depth. 
 
-lambda x: [y for l in x for y in flatten(l)] if type(x) is list else [x]
+flatten = lambda x: [y for l in x for y in flatten(l)] if type(x) is list else [x]
+
