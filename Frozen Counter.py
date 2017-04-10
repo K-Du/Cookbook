28aaace925a6f3c86d5f19fa8c0941e485e64a29
Kevin Du <kevinkldu@gmail.com>
2017-04-09 21:39:05 -0700

    Create Frozen Counter.py

---
+++ Frozen Counter.py
@@ -0,0 +1,7 @@
+# Gives the Counter class a hash attribute
+
+from collections import Counter
+
+class FrozenCounter(Counter):
+    def __hash__(self):
+        return hash(frozenset(self.items())))
